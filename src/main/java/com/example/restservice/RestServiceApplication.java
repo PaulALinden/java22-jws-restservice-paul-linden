99a733f8f6d20a1c624e6e6bc7bbd25566d68b83
Paul <paul.linden@gritacademy.se>
2023-03-27 17:34:26 +0200

    Changed filepath to data.json

--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -2,12 +2,10 @@ package com.example.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class RestServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestServiceApplication.class, args);
+	SpringApplication.run(RestServiceApplication.class, args);
 	}
-
 }
--- src/main/java/com/example/restservice/model/JsonManager.java
+++ src/main/java/com/example/restservice/model/JsonManager.java
@@ -1,18 +1,22 @@
 package com.example.restservice.model;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class JsonManager {
+
     private final ArrayList<Integer> idList = new ArrayList<>();
     private final ArrayList<String> contentList = new ArrayList<>();
 
     public void addJson() throws IOException, ParseException {
-        Object data = new JSONParser().parse(new FileReader("C:\\Users\\linde\\Downloads\\rest-service\\rest-service\\src\\main\\java\\com\\example\\restservice\\data.json"));
+
+        Object data = new JSONParser().parse(new FileReader("rest-service/src/main/java/com/example/restservice/data/data.json"));
         JSONArray jsonData = (JSONArray) data;
         for(Object obj : jsonData) {
             JSONObject jsonObject = (JSONObject) obj;
